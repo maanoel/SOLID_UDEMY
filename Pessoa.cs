85852fa1f9f4f5d718e1a6ef2ecd2f24b0681f07
maanoel <manoelvitorbrito@gmail.com>
2022-08-17 13:33:04 -0300

    [FEAT] Creating repository of SOLID course UDEMY

---
+++ Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace SOLID_UDEMY
+{
+  partial class Program
+  {
+    class Pessoa {
+      //field 
+      public string Nome;
+      public string Genero;
+
+      public void Identificar() {
+        System.Console.WriteLine($"Olá {Nome} do sexo {Genero}");
+      }
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SOLID_UDEMY
+{
+  partial class Program
+  {
+    static void Main(string[] args)
+    {
+      Pessoa pessoa = new Pessoa();
+      pessoa.Nome = "Rafa";
+      pessoa.Genero = "feminino";
+
+      pessoa.Identificar();
+
+      Console.WriteLine(pessoa.Nome);
+    }
+  }
+}
