d980ed149fa140fd2630075c7d9616359cb4ee6b
maanoel <manoelvitorbrito@gmail.com>
2022-08-19 20:10:52 -0300

    [FEAT] Rich clientes

---
+++ ClienteRico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SOLID_UDEMY
+{
+  class ClienteRico
+  {
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Endereco { get; private set; }
+
+
+    public ClienteRico(int id, string nome, string endereco)
+    {
+      if (id <= 0) throw new InvalidOperationException("Não pode ser menor que zero.");
+
+      Id = id;
+      Nome = nome;
+      Endereco = endereco;
+    }
+  }
+}
