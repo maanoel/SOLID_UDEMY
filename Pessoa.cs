cdf58cd89f4453fa2d546858a985731a8fec4749
maanoel <manoelvitorbrito@gmail.com>
2022-08-19 18:46:36 -0300

    [FEAT] Testing many posibilties with properties

--- Pessoa.cs
+++ Pessoa.cs
@@ -2,13 +2,32 @@
 {
   partial class Program
   {
-    class Pessoa {
-      //field 
-      public string Nome;
-      public string Genero;
+    class Pessoa
+    {
+      //public string nome;
 
-      public void Identificar() {
-        System.Console.WriteLine($"Olá {Nome} do sexo {Genero}");
+      //public string Nome
+      //{
+      //  get { return nome; }
+      //  set { nome = value; }
+      //}
+
+      public string Genero;
+      private int _idade;
+
+      public int Idade
+      {
+        get { return _idade; }
+        set { if (value > 18) { System.Console.WriteLine("Maluco é maior de idade"); _idade = value; } }
+      }
+
+      public string Nome { get; set; }
+
+      public void Identificar()
+      {
+        Idade = 10;
+
+        System.Console.WriteLine($"Olá {Nome} do sexo {Genero} {Idade}");
       }
     }
   }
