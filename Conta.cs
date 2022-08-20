0720437bf15d781ed28fe62f7f01383c64115c2c
maanoel <manoelvitorbrito@gmail.com>
2022-08-20 10:11:47 -0300

    [FEAT] Method hiding in c#, we use the new keyword in method sign

---
+++ Conta.cs
@@ -0,0 +1,14 @@
+﻿namespace SOLID_UDEMY
+{
+  public class Conta
+  {
+    public decimal Saldo { get; protected set; }
+
+    public void Depositar(decimal valor)
+    {
+      if (valor <= 0) return;
+
+      Saldo += valor;
+    }
+  }
+}
---
+++ ContaPoupanca.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID_UDEMY
+{
+  public class ContaPoupanca : Conta
+  {
+    public new void Depositar(decimal valor)
+    {
+      if (Saldo <= 0) return;
+
+      Saldo = valor;
+    }
+  }
+}
